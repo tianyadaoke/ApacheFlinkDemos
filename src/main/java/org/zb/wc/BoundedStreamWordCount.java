ea3dcaa3156de79c9fb4cbf523e395fb71676a10
Bing Zhou <bingfbi@gmail.com>
2022-03-23 12:01:15 +0100

    data stream source

---
+++ src/main/java/org/zb/wc/BoundedStreamWordCount.java
@@ -0,0 +1,28 @@
+package org.zb.wc;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class BoundedStreamWordCount {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> streamSource = environment.readTextFile("input/words.txt");
+        SingleOutputStreamOperator<Tuple2<String, Long>> wordAndLongTuple = streamSource.flatMap((String line, Collector<Tuple2<String, Long>> collector) -> {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        collector.collect(Tuple2.of(word, 1L));
+                    }
+                })
+                .returns(Types.TUPLE(Types.STRING, Types.LONG));
+        KeyedStream<Tuple2<String, Long>, String> keyedStream = wordAndLongTuple.keyBy(data -> data.f0);
+        SingleOutputStreamOperator<Tuple2<String, Long>> sum = keyedStream.sum(1);
+        sum.print();
+        environment.execute();
+
+    }
+}
